860ac1b38876806aef53507cde609ba5076bc3e6
Alyson Gadelha <alyson_sud@hotmail.com>
2023-02-03 19:47:54 -0300

    improve styles and images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Coming_Soon } from "@next/font/google";
+
+const comingSoon = Coming_Soon({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-soon",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${comingSoon.variable} font-sans`}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
